7531615cf10c6749c7f8216487c380c75484c435
radomirKrastev <49518363+radomirKrastev@users.noreply.github.com>
2021-01-31 14:11:40 +0200

    change deprecated function #1

--- server/src/data/userData.js
+++ server/src/data/userData.js
@@ -3,11 +3,11 @@ const { getDb } = require('./db');
 module.exports = {
     create: async userDocument => {
         await getDb().collection('users').insertOne(userDocument);
-        
+
         return userDocument;
     },
 
     checkIsUsernameOccupied: async username => {
-        return await getDb().collection('users').count({ username }, { limit: 1 });
+        return await getDb().collection('users').countDocuments({ username }, { limit: 1 });
     }
 };
